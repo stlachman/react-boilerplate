b8acc94f02599f52c6b90239445a1a93165f821c
Seth Lachman <lachmanseth17@gmail.com>
2020-02-28 09:38:00 -0500

    Added basic tests.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,28 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: "babel-loader",
-        exclude: /node-modules/,
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets: [
-                  "last 2 versions",
-                  "not dead",
-                  "not < 2%",
-                  "not ie 11"
-                ]
-              }
-            ],
-            "@babel/preset-react"
-          ],
-          plugins: [
-            "react-hot-loader/babel",
-            "@babel/plugin-proposal-class-properties",
-            "@babel/plugin-syntax-dynamic-import"
-          ]
-        }
+        exclude: /node-modules/
       },
       {
         test: /\.css$/,
