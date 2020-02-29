a01dc42e25267ec4f999ac768133749366ae1210
Seth Lachman <lachmanseth17@gmail.com>
2020-02-28 20:35:28 -0500

    Cleaned up the code.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "@babel/polyfill";
 import App from "./App";
+import DefaultErrorBoundary from "./DefaultErrorBoundary";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+if (process.env.NODE_ENV === "development") {
+  const axe = require("react-axe");
+  axe(React, ReactDOM, 1000);
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById("app")
+);
